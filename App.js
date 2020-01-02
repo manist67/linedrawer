e57b3d8ad9651c72bfca8ee49f001d9796a8a47a
Minsung Kim <minsungkim@perfitt.io>
2020-01-02 23:37:51 +0900

    FISRT COMMIT

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+import Liner from './components/liner';
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Liner></Liner>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ components/Liner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View} from 'react-native';
+import Svg, {} from 'react-native-svg';
+
+function Liner() { 
+    const [items, setItems] = React.useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    const [currentIdx, setCurrentIdx] = React.useState(0);
+
+    return (
+        <View>
+            <Svg height="50%" width="100%">
+        
+            </Svg>
+        </View>
+    )
+}
+
+export default Liner;
